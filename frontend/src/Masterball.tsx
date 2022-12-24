01b9ee6b8371c4984efb58e2efcdc0771c1bf0fe
tensa2205 <borlandi_diego@hotmail.com>
2022-12-23 21:45:56 -0300

    Masterball with animations

--- frontend/src/Masterball.tsx
+++ frontend/src/Masterball.tsx
@@ -1,14 +1,29 @@
 import './masterball.css';
+import { motion } from "framer-motion"
 import React from 'react';
 
-export default function Masterball() {
-    const [show, setShow] = React.useState(true); //Hacer la animación con una libreria, la idea es que la pokebola se caiga de la pantalla, que se desmonte el componente y que se le pegue al back.
+interface Props {
+    onOpen: Function;
+}
+export default function Masterball(props : Props) {
+    const [endY, setEndY] = React.useState(0); //Hacer la animación con una libreria, la idea es que la pokebola se caiga de la pantalla, que se desmonte el componente y que se le pegue al back.
+    const [rotation, setRotation] = React.useState(0);
+    const [opacity, setOpacity] = React.useState(1);
+    
     const openMasterball = () => {
-        setShow(false);
+        setEndY(1000);
+        setRotation(360);
+
+        setTimeout(disappear, 1500);
+    }
+
+    const disappear = () => {
+        props.onOpen();
+        setOpacity(0);
     }
     return (
-        <section className={show ? 'show' : 'hide'}>
-            <section>
+    <motion.div className='no-background-color' animate={{ y: [0, endY], rotate: rotation}} transition={{duration: 2}}>
+        <motion.section className='no-background-color' animate={{opacity: opacity}} transition={{duration: .5}}>
                 <div className="one"></div>
                 <div className="two"></div>
                 <div className="four"></div>
@@ -18,8 +33,8 @@ export default function Masterball() {
                 <div className="eight">.</div>
                 <div onClick={ openMasterball } className="nine">.</div>
                 <h1 className="masterball-name">w</h1>
-            </section>
             <div className="three"></div>
-        </section>
+        </motion.section>
+    </motion.div>
     )
 }
