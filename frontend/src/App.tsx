8f6e1630cb52685884a71838b3d4274fb9c36a11
tensa2205 <borlandi_diego@hotmail.com>
2023-01-13 16:15:48 -0300

    Name filter on front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,30 +8,58 @@ import PaginateCards from './PaginateCards';
 function App() {
   const [loading, setLoading] = React.useState(true);
   const [pokemons, setPokemons] = React.useState<Array<Pokemon>>([]);
+  const [findedPokemons, setFindedPokemons] = React.useState<Array<Pokemon>>([]);
+  const [searchedName, setSearchedName] = React.useState("");
   
   useEffect(() => {
     document.body.style.overflow = "hidden";
     document.body.style.backgroundColor = "#FDF5BF";
   }, [])
 
+  //Debounced useEffect
+  useEffect(() => {
+    const getResult = setTimeout(() => {
+      if (searchedName === '') {
+        setPokemons(pokemons);
+        setFindedPokemons([]);
+      } else {
+        setFindedPokemons(pokemons.filter(pkm => pkm.name.toLowerCase().includes(searchedName)));
+      }
+    }, 750);
+    return () => clearTimeout(getResult);
+  }, [pokemons, searchedName])
+
   const onOpen = () => {
-    setLoading(false);
     retrieveAllPokemons();
   }
+
 
   const retrieveAllPokemons = () => {
     PokemonService.getAllPokemons().then((res) => {
       setPokemons(res.map(pkm => JSON.parse(pkm)));
-  })
+      setLoading(false);
+    })
+  }
+
+  const onChange = (event : React.ChangeEvent<HTMLInputElement>) => {
+    setSearchedName(event.target.value);
   }
 
   return (
     <>
-      <header className="header"></header>
+      <header className="header">
+        <div className="page-title">Pokedex</div>
+      { !loading && 
+          <div className='search-bar'>
+            <label htmlFor="fname">Search by name: </label>
+            <input type="text" id="pname" name="pname" onChange={onChange}/>
+          </div>
+        }
+      </header>
       { loading && <Masterball onOpen={onOpen}/> }
 
-      { !loading && 
-        <PaginateCards pokemons={pokemons}/>
+      { !loading &&
+        <PaginateCards pokemons={ findedPokemons.length ? findedPokemons : pokemons}/>
       }
     </>
   );
