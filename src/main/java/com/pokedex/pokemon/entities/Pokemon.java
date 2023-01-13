8e0d799db4210a6a4d5c3980d2712b0b34cec026
tensa2205 <borlandi_diego@hotmail.com>
2023-01-13 16:15:07 -0300

    Many to Many relation added on Pokemon

--- src/main/java/com/pokedex/pokemon/entities/Pokemon.java
+++ src/main/java/com/pokedex/pokemon/entities/Pokemon.java
@@ -2,11 +2,17 @@ package com.pokedex.pokemon.entities;
 
 import com.pokedex.pokemon.dto.PokemonDTO;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,16 +32,32 @@ public class Pokemon implements Serializable {
     private Long pokemonNumber;
     private String imageUrl;
     
-    public static Pokemon fromDTO(PokemonDTO dto) {
+    @ManyToMany
+    //JoinTable is used for the Owner side of the relationship. https://www.baeldung.com/jpa-many-to-many
+    @JoinTable(
+            name = "pokemon_has_type",
+            joinColumns = @JoinColumn(name = "pokemon_id"),
+            inverseJoinColumns = @JoinColumn(name = "type_id")
+    )
+    private Set<Type> types;
+    
+    public static Pokemon fromDTO(PokemonDTO dto, Set<Type> types) {
         return Pokemon.builder()
                 .name(dto.getName())
                 .pokemonNumber(dto.getId())
                 .imageUrl(dto.getImageUrl())
+                .types(types)
                 .build();
     }
     
     public PokemonDTO toDTO() {
-       return new PokemonDTO(this.getPokemonNumber(), this.getName(), this.getImageUrl());
+       return new PokemonDTO(this.getPokemonNumber(), this.getName(), this.getImageUrl(), this.getTypesAsString());
+    }
+    
+    public Set<String> getTypesAsString() {
+        Set<String> typesStr = new HashSet();
+        this.getTypes().forEach(type -> typesStr.add(type.getName()));
+        return typesStr;
     }
     
     @Override
