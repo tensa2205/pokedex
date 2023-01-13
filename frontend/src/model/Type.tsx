61683dd32d2a1c7bc1a0d0fc29d50cb833f7333b
tensa2205 <borlandi_diego@hotmail.com>
2023-01-13 16:15:26 -0300

    Type enum

---
+++ frontend/src/model/Type.tsx
@@ -0,0 +1,20 @@
+export enum Type {
+    NORMAL,
+    FIRE,
+    WATER,
+    GRASS,
+    FLYING,
+    FIGHTING,
+    POISON,
+    ELECTRIC,
+    GROUND,
+    ROCK,
+    PSYCHIC,
+    ICE,
+    BUG,
+    GHOST,
+    STEEL,
+    DRAGON,
+    DARK,
+    FAIRY
+}
