dbc72d37da9593fc39f93eed8cb5a50b313f3d61
tensa2205 <borlandi_diego@hotmail.com>
2023-02-07 17:12:50 -0300

    New library, added type selector and fixed search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,64 @@
 import React, { useEffect } from 'react';
+import Select from 'react-select'
 import './App.css';
 import PokemonService from './client/PokemonService';
 import Masterball from './Masterball';
 import Pokemon from './model/Pokemon';
+import { Type } from './model/Type';
 import PaginateCards from './PaginateCards';
 
+function capitalize(s : string) {
+  return s[0].toUpperCase() + s.slice(1);
+}
+
+interface Option {
+  value: string,
+  label: string
+}
+
+const generateOptions = () : Array<Option> => {
+  const types = Object.keys(Type);
+  return types.map((type) => ( { value: type, label: capitalize(type.toLowerCase())} ))
+}
 function App() {
   const [loading, setLoading] = React.useState(true);
   const [pokemons, setPokemons] = React.useState<Array<Pokemon>>([]);
   const [findedPokemons, setFindedPokemons] = React.useState<Array<Pokemon>>([]);
   const [searchedName, setSearchedName] = React.useState("");
+  const [selectedTypes, setSelectedTypes] = React.useState<Array<string>>([]);
+  const options = generateOptions();
   
   useEffect(() => {
     document.body.style.overflow = "hidden";
     document.body.style.backgroundColor = "#FDF5BF";
   }, [])
 
-  //TODO - FIX SEARCH THAT IS NOT WORKING.
+
   //Debounced useEffect
   useEffect(() => {
+    if (loading) return;
     const getResult = setTimeout(() => {
-      if (searchedName === '') {
-        setPokemons(pokemons);
-        setFindedPokemons([]);
+      if (searchedName === '' && selectedTypes.length === 0) {
+        setFindedPokemons(pokemons);
       } else {
-        setFindedPokemons(pokemons.filter(pkm => pkm.name.toLowerCase().includes(searchedName)));
+        let tempList = pokemons;
+        if (selectedTypes.length !== 0) {
+          tempList = tempList.filter(pkm => anyTypeIncludes(pkm.types));
+        }
+        if (searchedName !== '') {
+          tempList = tempList.filter(pkm => pkm.name.toLowerCase().includes(searchedName.toLowerCase()));
+        }
+        setFindedPokemons(tempList);
       }
     }, 750);
     return () => clearTimeout(getResult);
-  }, [pokemons, searchedName])
+  }, [searchedName, selectedTypes, pokemons, loading])
+
+  const anyTypeIncludes = (types : Array<Type>) : boolean => {
+    const typesAsStr = types.map(type => type.toString());
+    const found = selectedTypes.every(selectedType => typesAsStr.indexOf(selectedType) >= 0);
+    return found;
+  } 
 
   const onOpen = () => {
     retrieveAllPokemons();
@@ -46,21 +76,33 @@ function App() {
     setSearchedName(event.target.value);
   }
 
+  const onTypeSelect = (newValue: any) => {
+    const selected : Array<Option> = newValue;
+    if (selected.length) {
+      setSelectedTypes(selected.map(option => option.value));
+    } else {
+      setSelectedTypes([])
+    }
+  }
+
   return (
     <>
-      <header className="header">
+      <div className="header" style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
         <div className="page-title">Pokedex</div>
       { !loading && 
+          <>
           <div className='search-bar'>
             <label htmlFor="fname">Search by name: </label>
             <input type="text" id="pname" name="pname" onChange={onChange}/>
           </div>
+          <Select className='search-select' options={options} onChange={onTypeSelect} isMulti/>
+          </>
         }
-      </header>
+      </div>
       { loading && <Masterball onOpen={onOpen}/> }
 
       { !loading &&
-        <PaginateCards pokemons={ findedPokemons.length ? findedPokemons : pokemons}/>
+        <PaginateCards pokemons={findedPokemons}/>
       }
     </>
   );
